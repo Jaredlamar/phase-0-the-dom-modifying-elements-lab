a980d6330c52abc06d8eaa68bd7a4f025e8309b1
Jared <redlaward@gmail.com>
2022-02-28 22:33:09 -0500

    commit tree 71b58115cd782ac03e5aa9e33dd8e35c2f3335c2

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your code here!
+
+let removeMain = document.querySelector('main')
+removeMain.remove()
+
+let newHeader = document.createElement('h1')
+
+newHeader.id = 'victory'
+newHeader.textContent = 'Jared is the champion'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//document.getElementById('main')
+//other option:document.querySelector("main#main").remove();
+
+//const main = document.querySelector('#main')
+//main.remove()
+
+//const newHeader = document.createElement('h1')
+//newHeader.id = "victory"
+//newHeader.textContent = "Jared is the champion"
+
+
+
+
+//const body = document.getElementsByTagName('body')
+//const removeMain = main.querySelector('main')
+//body.removeChild(removeMain)
